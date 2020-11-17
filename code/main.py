822722bac45ee501b0220119cd33dc4904ae543c
AdamPBerkley <56803674+AdamPBerkley@users.noreply.github.com>
2020-11-17 15:32:47 -0500

    simple preprocess, but not for kaggle dataset

---
+++ code/main.py
@@ -0,0 +1,16 @@
+import numpy as np
+import tensorflow as tf
+import os
+
+from preprocess import get_data_main
+
+
+
+def main():
+	path = '../data/main_dataset/'
+	train_data, train_labels = get_data_main(path + 'train/')
+	test_data, test_labels = get_data_main(path + 'test/')
+
+
+if __name__ == '__main__':
+	main()
