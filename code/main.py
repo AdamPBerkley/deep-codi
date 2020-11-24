83a473c177a7a97ead046670fd5ddf172d5a5374
Matthew Rodrigues <matt@mrod.io>
2020-11-24 16:45:46 -0500

    chore: remove unnecessary code

--- code/main.py
+++ code/main.py
@@ -9,19 +9,8 @@ from PIL import Image
 def main():
 	path = '../data/main_dataset/'
 	train_data, train_labels = get_data_main(path + 'train/')
-	# test_data, test_labels = get_data_main(path + 'test/')
-
-	jpg = train_data[0]
-	png = train_data[1]
-	print(np.array_equal(jpg, png))
-	print(np.sum(jpg - png))
+	test_data, test_labels = get_data_main(path + 'test/')
 	
-	# from matplotlib import pyplot as plt
-	# plt.imshow(train_data[0], interpolation='nearest')
-	# plt.show()
-
-	# plt.imshow(train_data[2], interpolation='nearest')
-	# plt.show()
 
 if __name__ == '__main__':
 	main()
