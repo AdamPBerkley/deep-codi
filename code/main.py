e89c9e183aa973764e41717d250bef92adbd28b4
Anon <57374637+ccataldo880@users.noreply.github.com>
2020-11-28 15:47:32 -0500

    Implement default VGG implementation
    Still needs param adjustment for model to run - framework for now

--- code/main.py
+++ code/main.py
@@ -5,12 +5,61 @@ import os
 from preprocess import get_data_main
 from PIL import Image
 
+batch_size = 20
+threshold = .1 #below = not covid, above = covid
+
+def test(model,test_data):
+    preds = model.predict(test_data,batch_size = batch_size)
+    return preds
+
+def specificty(test_labels,predictions,threshold):
+    """
+    returns: float that represents # of correctly predicted non-COVID images over the total number of non-COVID images
+    """
+    correct = 0
+    total = 0
+    for i in range(len(predictions)):
+        if np.argmax(test_labels[i]) == 0: #Non-COVID image
+            total += 0
+            if predictions[i] < threshold: #Correct prediction of Non-COVID Image
+                correct +=1
+    return correct/total
+def sensitivity(test_labels,predictions,threshold):
+    """
+    returns: float that represents # of correctly predicted COVID images over the total number of COVID images
+    """
+    correct = 0
+    total = 0
+    for i in range(len(predictions)):
+        if np.argmax(test_labels[i]) == 1: #COVID image
+            total += 0
+            if predictions[i] >= threshold: #Correct prediction of COVID Image
+                correct +=1
+    return correct/total
 
 def main():
-	path = '../data/main_dataset/'
-	train_data, train_labels = get_data_main(path + 'train/')
-	test_data, test_labels = get_data_main(path + 'test/')
-	
+    path = '../data/main_dataset/'
+    print("Loading the data...")
+    train_data, train_labels = get_data_main(path + 'train/')
+    test_data, test_labels = get_data_main(path + 'test/')
+    
+    print("Generating the model...")
+    vgg16 = tf.keras.applications.VGG16(include_top=True, weights="imagenet")
 
+    sensitivity_list = []
+    specificity_list = []
+    
+    print("Testing...")
+    for start, end in zip(range(0, len(test_data) - batch_size, batch_size), range(batch_size, len(test_data), batch_size)):
+        test_batch = np.expand_dims(test_data[start:end],axis=0)
+        batch_labels = test_labels[start:end]       
+        predictions = test(vgg16,test_batch)
+        sensitivity_list.append(sensitivity(batch_labels,predictions,threshold))
+        specificity_list.append(specificity(batch_labels,predictions,threshold))
+    tot_spec = np.mean(specificity_list)
+    tot_sens = np.mean(sensitivity_list)
+    print('Avg Specificity: ', tot_spec)
+    print('Avg Sensitivity: ', tot_sens)
+    
 if __name__ == '__main__':
-	main()
+    main()
--- code/preprocess.py
+++ code/preprocess.py
@@ -7,50 +7,50 @@ import glob
 from PIL import Image
 
 def normalize_image(image):
-	image = image/np.max(image)
-	return image
+    image = image/np.max(image)
+    return image
 
 
 def get_data_main(path, imsize=256):
-	"""
-	Given a file path, returns an array of normalized inputs (images) and an array of 
-	one_hot encoded binary labels. 
+    """
+    Given a file path, returns an array of normalized inputs (images) and an array of 
+    one_hot encoded binary labels. 
 
-	:param path: file path for inputs and labels, something 
-	like '../data/main_dataset/train/'
-	:param imsie:  an integer  representing how many pixels you want in your 
-	image for the dataset. uses Pillow for automatic scaling to correct size 
-	:return:NumPy array of normalized inputs of labels, where 
-	inputs are black and white (one channel) and of size imsize x imsize	"""
-	covid_pics = glob.glob(path+"covid/*")
-	if 'test' in path:
-		non_pics = glob.glob(path+"non/**/*")
-	else:
-		non_pics = glob.glob(path+"non/*")
-	num_pics = len(covid_pics)+len(non_pics)
-	data = np.empty((num_pics, imsize, imsize))
-	labels = np.zeros((num_pics, 2))
-	index = 0
-	sizes = []
-	for pic in covid_pics:
-		image = Image.open(pic).convert('L').resize((imsize,imsize))
-		sizes.append(image.size)
-		im_data = np.asarray(image)
-		data[index] = normalize_image(im_data)
-		labels[index,1] = 1
-		index += 1
-	for pic in non_pics:
-		image = Image.open(pic).convert('L').resize((imsize,imsize))
-		sizes.append(image.size)
-		im_data = np.asarray(image)
-		data[index] = normalize_image(im_data)
-		labels[index,0] = 1
-		index += 1
+    :param path: file path for inputs and labels, something 
+    like '../data/main_dataset/train/'
+    :param imsie:  an integer  representing how many pixels you want in your 
+    image for the dataset. uses Pillow for automatic scaling to correct size 
+    :return:NumPy array of normalized inputs of labels, where 
+    inputs are black and white (one channel) and of size imsize x imsize    """
+    covid_pics = glob.glob(path+"covid/*")
+    if 'test' in path:
+        non_pics = glob.glob(path+"non/**/*")
+    else:
+        non_pics = glob.glob(path+"non/*")
+    num_pics = len(covid_pics)+len(non_pics)
+    data = np.empty((num_pics, imsize, imsize))
+    labels = np.zeros((num_pics, 2))
+    index = 0
+    sizes = []
+    for pic in covid_pics:
+        image = Image.open(pic).convert('L').resize((imsize,imsize))
+        sizes.append(image.size)
+        im_data = np.asarray(image)
+        data[index] = normalize_image(np.expand_dims(im_data,axis=0))
+        labels[index,1] = 1
+        index += 1
+    for pic in non_pics:
+        image = Image.open(pic).convert('L').resize((imsize,imsize))
+        sizes.append(image.size)
+        im_data = np.asarray(image)
+        data[index] = normalize_image(np.expand_dims(im_data,axis=0))
+        labels[index,0] = 1
+        index += 1
 
-	return data, labels
+    return data, labels
 
 
 if __name__ == '__main__':
-	path1 = '../data/kaggle_dataset/train/*'
-	path2 = '../data/main_dataset/train/'
-	data, labels = get_data_main(path2)
+    path1 = '../data/kaggle_dataset/train/*'
+    path2 = '../data/main_dataset/train/'
+    data, labels = get_data_main(path2)
